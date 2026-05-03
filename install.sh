8a13b38e92e6a357a00814ceca1418badadb9c26
napainfra <napa.infra@gmail.com>
2026-05-03 03:48:25 +0000

    lowercase-only install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -e
-echo "[1/5] Removing old folder..."
+echo "removing old folder..."
 rm -rf /addons/garden_pwa
-echo "[2/5] Downloading v1.0.1..."
 cd /addons
+echo "downloading zip..."
 wget -q https://raw.githubusercontent.com/napainfra/garden-pwa-deploy/main/garden-pwa-addon.zip
-echo "[3/5] Unzipping..."
+echo "unzipping..."
 unzip -oq garden-pwa-addon.zip
 rm garden-pwa-addon.zip
-echo "[4/5] Files in /addons/garden_pwa:"
+echo ""
+echo "==== files ===="
 ls /addons/garden_pwa
-echo "[5/5] Version:"
+echo ""
+echo "==== version ===="
 head -3 /addons/garden_pwa/config.yaml
 echo ""
-echo "DONE. Now in HA UI: Settings -> Apps -> top-right 3-dot menu -> Check for updates"
+echo "DONE"
